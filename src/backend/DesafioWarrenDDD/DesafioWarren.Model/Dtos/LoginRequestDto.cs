429e15f281f4d6dc4173f964678f40db5e66c276
Tauan Matos <tauan.matos164@gmail.com>
2020-07-30 19:13:57 -0300

    Criação login dto

--- src/backend/DesafioWarrenDDD/DesafioWarren.Model/Dtos/LoginRequestDto.cs
+++ src/backend/DesafioWarrenDDD/DesafioWarren.Model/Dtos/LoginRequestDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DesafioWarren.Model.Dtos
 {
-    public class UserDto
+    public class LoginRequestDto
     {
         public string UserName { get; set; }
         public string Password { get; set; }

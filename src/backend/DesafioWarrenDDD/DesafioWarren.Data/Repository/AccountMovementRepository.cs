9f8deb7c1d524de52cf725a7fc74f6198463eb47
Tauan Matos <tauan.matos164@gmail.com>
2020-07-31 21:09:50 -0300

    Métodos repository

--- src/backend/DesafioWarrenDDD/DesafioWarren.Data/Repository/AccountMovementRepository.cs
+++ src/backend/DesafioWarrenDDD/DesafioWarren.Data/Repository/AccountMovementRepository.cs
@@ -3,6 +3,7 @@ using DesafioWarren.Model.Entities;
 using DesafioWarren.Model.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DesafioWarren.Data.Repository
@@ -11,6 +12,28 @@ namespace DesafioWarren.Data.Repository
     {
         public AccountMovementRepository(WarrenDbContext warrenDbContext) : base(warrenDbContext)
         {
+        }
+
+
+        public void Remove(int id) => base.Delete(id);
+
+
+        public void Save(AccountMovement obj)
+        {
+            if (obj.Id == 0)
+                base.Insert(obj);
+            else
+                base.Update(obj);
+        }
+
+        public AccountMovement GetById(int id) => base.Select(id);
+
+        public IList<AccountMovement> GetAll() => base.Select();
+
+        public IList<AccountMovement> GetAccountMovementByAccountId(int accountId)
+        {
+            var accountMovementsList = this._warrenDbContext.AccountMovements.Where(a => a.AccountId == accountId).ToList();
+            return accountMovementsList;
         }
     }
 }
--- src/backend/DesafioWarrenDDD/DesafioWarren.Data/Repository/AccountRepository.cs
+++ src/backend/DesafioWarrenDDD/DesafioWarren.Data/Repository/AccountRepository.cs
@@ -1,8 +1,10 @@
 ﻿using DesafioWarren.Data.Context;
 using DesafioWarren.Model.Entities;
 using DesafioWarren.Model.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DesafioWarren.Data.Repository
@@ -12,5 +14,18 @@ namespace DesafioWarren.Data.Repository
         public AccountRepository(WarrenDbContext warrenDbContext) : base(warrenDbContext)
         {
         }
+
+
+        public void Remove(int id) => base.Delete(id);
+
+        public void Save(Account obj) => base.Update(obj);
+
+        public Account GetById(int id)
+        {
+            var account = _warrenDbContext.Accounts.Include(account => account.Client).Where(a => a.Id == id).FirstOrDefault();
+            return account;
+        }
+
+        public IList<Account> GetAll() => base.Select();
     }
 }

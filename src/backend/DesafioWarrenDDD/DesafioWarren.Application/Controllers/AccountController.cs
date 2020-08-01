f383afd49f8ad43117247d3dc01a262f9619eda0
Tauan Matos <tauan.matos164@gmail.com>
2020-07-31 21:10:01 -0300

    Requests do controller

--- src/backend/DesafioWarrenDDD/DesafioWarren.Application/Controllers/AccountController.cs
+++ src/backend/DesafioWarrenDDD/DesafioWarren.Application/Controllers/AccountController.cs
@@ -2,16 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DesafioWarren.Model.Dtos;
+using DesafioWarren.Model.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DesafioWarren.Application.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     [Authorize("Bearer")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public class AccountController : ControllerBase
     {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            this._accountService = accountService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<AccountResponseDto> Deposit([FromBody] AccountRequestDto accountRequest)
+        {
+            try
+            {
+                var response = this._accountService.Deposit(accountRequest);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<AccountResponseDto> Withdraw([FromBody] AccountRequestDto accountRequest)
+        {
+            try
+            {
+                var response = this._accountService.Withdraw(accountRequest);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<AccountResponseDto> Payment([FromBody] AccountRequestDto accountRequest)
+        {
+            try
+            {
+                var response = this._accountService.Payment(accountRequest);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
     }
 }
--- src/backend/DesafioWarrenDDD/DesafioWarren.Application/Controllers/AccountHistoryController.cs
+++ src/backend/DesafioWarrenDDD/DesafioWarren.Application/Controllers/AccountHistoryController.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DesafioWarren.Model.Entities;
+using DesafioWarren.Model.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DesafioWarren.Application.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     [Authorize("Bearer")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public class AccountHistoryController : ControllerBase
     {
+        private readonly IAccountMovementService _accountMovementService;
+        public AccountHistoryController(IAccountMovementService accountMovementService)
+        {
+            this._accountMovementService = accountMovementService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<IList<AccountMovement>> GetAccountHistory(int id)
+        {
+            try
+            {
+                var accountMovements = this._accountMovementService.GetAccountMovement(id);
+                return Ok(accountMovements);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
     }
 }

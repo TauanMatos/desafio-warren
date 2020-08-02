7abd7c77917435d405e0df67512cda675914e868
Tauan Matos <tauan.matos164@gmail.com>
2020-08-01 23:27:35 -0300

    criação componentes

---
+++ src/frontend/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+
+@Component({
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+  })
+export class HomeComponent {
+    loading = false;
+ 
+
+    constructor() { }
+
+    ngOnInit() {
+        this.loading = true;
+
+    }
+}
